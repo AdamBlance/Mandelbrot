257cf038157c0525de6521a52d7b239479c68d32
adam <adam.blance@gmail.com>
2016-09-25 21:18:56 +0100

    Separated the OpenCL from main code.

--- PyOpenCL.py
+++ PyOpenCL.py
@@ -23,40 +23,9 @@ def get_iterations(context, complex_values, iterations):
     complex_values_buffer = cl.Buffer(context, flags.READ_ONLY | flags.COPY_HOST_PTR, hostbuf=complex_values)
     output_array_buffer = cl.Buffer(context, flags.WRITE_ONLY, output_array.nbytes)
 
-    program = cl.Program(context, '''
-    #pragma OPENCL EXTENSION cl_khr_fp64 : enable
-    __kernel void mandelbrot(global double2 *complex_values,
-                             global uint *output_array,
-                             global uint *gradient,
-                             ushort const iterations)
-    {
-        uint id = get_global_id(0);
-
-        double zx = 0;
-        double zy = 0;
-        double cx = complex_values[id].x;
-        double cy = complex_values[id].y;
-        double inv_log_2 = 1/log(2.0f);
-
-        output_array[id] = 0;
-
-        for(uint i = 0; i <= iterations; i++){
-            double zx2 = zx*zx;
-            double zy2 = zy*zy;
-            double zx2_plus_zy2 = zx2 + zy2;
-
-            double temp_zx = zx2 - zy2 + cx;
-            zy = 2*zx*zy + cy;
-            zx = temp_zx;
-            if (zx2_plus_zy2 > 4.0f){
-                double grad = log(log(sqrt(zx2_plus_zy2)) * inv_log_2) * inv_log_2;
-                ushort colour = (int)(sqrt(i + 1 - grad) * 256) % 2048;
-                output_array[id] = gradient[colour];
-                return;
-            }
-        }
-    }
-    ''').build()
+    test = open('kernel.cl', 'r')
+    program = cl.Program(context, test.read()).build()
+    test.close()
 
     program.mandelbrot(command_queue,
                        complex_values.shape,
@@ -67,6 +36,7 @@ def get_iterations(context, complex_values, iterations):
                        np.ushort(iterations))
 
     cl.enqueue_read_buffer(command_queue, output_array_buffer, output_array).wait()
+
     return output_array
 
 
@@ -90,7 +60,7 @@ def draw_mandelbrot(value_array):
 width = 1000
 height = 1000
 
-max_iterations = 2000
+max_iterations = 2500
 x_max = 2.3
 x_min = -2.3
 y_max = x_max*(height/width)
@@ -115,11 +85,33 @@ highlight_rect = pygame.Rect((0, 0), (0, 0))
 real_array = np.linspace(x_min, x_max, width, dtype=np.float64)
 imaginary_array = np.linspace(y_min, y_max, height, dtype=np.float64)
 
+focus = None
+for event in pygame.event.get():
+    if event.type == ACTIVEEVENT:
+        focus = False
+    else:
+        focus = True
+
 running = True
 clock = pygame.time.Clock()
 while running:
 
     clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        elif event.type == MOUSEBUTTONDOWN:
+            if mouse_pos_1 is None:
+                mouse_pos_1 = mouse_pos
+            elif mouse_pos_1 is not None:
+                if mouse_pos_2 is None:
+                    mouse_pos_2 = True
+        elif event.type == ACTIVEEVENT:
+            if focus:
+                focus = False
+            else:
+                focus = True
 
     mouse_pos = pygame.mouse.get_pos()
     coordinate_surface_x = coordinate_text.render('%s<x<%s' % (x_min, x_max), True, text_colour)
@@ -133,16 +125,6 @@ while running:
     main_surface.blit(coordinate_surface_x, (20, height - 50))
     main_surface.blit(coordinate_surface_y, (20, height - 30))
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-        elif event.type == MOUSEBUTTONDOWN:
-            if mouse_pos_1 is None:
-                mouse_pos_1 = mouse_pos
-            elif mouse_pos_1 is not None:
-                if mouse_pos_2 is None:
-                    mouse_pos_2 = True
-
     if mouse_pos_1 is not None:
         highlight_rect.x = mouse_pos_1[0]
         highlight_rect.y = mouse_pos_1[1]
@@ -153,6 +135,7 @@ while running:
             height_multiplier = -1
         highlight_rect.height = height_multiplier * abs(highlight_rect.width) * (height/width)
         pygame.draw.rect(main_surface, (255, 0, 0), highlight_rect, 2)
+        highlight_rect.normalize()
 
     if mouse_pos_1 and mouse_pos_2 is not None:
         mouse_pos_1 = None
---
+++ kernel.cl
@@ -0,0 +1,32 @@
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+__kernel void mandelbrot(global double2 *complex_values,
+                         global uint *output_array,
+                         global uint *gradient,
+                         ushort const iterations) {
+
+	uint id = get_global_id(0);
+
+	double zx = 0;
+	double zy = 0;
+	double cx = complex_values[id].x;
+	double cy = complex_values[id].y;
+	double inv_log_2 = 1/log(2.0f);
+
+	output_array[id] = 0;
+
+	for(uint i = 0; i <= iterations; i++) {
+		double zx2 = zx*zx;
+		double zy2 = zy*zy;
+		double zx2_plus_zy2 = zx2 + zy2;
+
+		double temp_zx = zx2 - zy2 + cx;
+		zy = 2*zx*zy + cy;
+		zx = temp_zx;
+		if (zx2_plus_zy2 > 4.0f){
+			double grad = log(log(sqrt(zx2_plus_zy2)) * inv_log_2) * inv_log_2;
+			ushort colour = (uint)(sqrt(i + 1 - grad) * 256) % 2048;
+			output_array[id] = gradient[colour];
+			return;
+		}
+	}
+}
